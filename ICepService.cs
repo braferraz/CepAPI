04aa0554ad3c322527646395d307e97cca585ea2
Braian Ferraz <braianfs2009@gmail.com>
2021-05-16 04:29:01 -0300

    Adicionar arquivos de projeto.

---
+++ ICepService.cs
@@ -0,0 +1,14 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CepAPI
+{
+    public interface ICepService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace CepAPI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var clientCep = RestService.For<ICepService>("https://viacep.com.br/");
+                Console.WriteLine("Informe seu CEP: ");
+                string cep = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando CEP ...");
+
+                var endereco = await clientCep.GetAddressAsync(cep);
+
+                Console.WriteLine("\nCEP: " + endereco.Cep);
+                Console.WriteLine("Endereço: " + endereco.Logradouro);
+                Console.WriteLine("Bairro: " + endereco.Bairro);
+                Console.WriteLine("Localidade: " + endereco.Localidade);
+
+                Console.WriteLine("\nPresione ENTER para sair...");
+                Console.Read();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro ao consultar CEP! Tente novamente!");
+            }
+        }
+    }
+}
